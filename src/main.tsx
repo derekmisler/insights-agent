ebb89a260e0346857d4ed02c2eb9a0ca5b7dd3ca
Derek Misler <derek.misler@docker.com>
2025-07-17 08:21:13 -0400

    Revert "Merge pull request #1 from derekmisler/ts"
    This reverts commit dc6806079199623fec8df1eea8edcd9d4fc762be, reversing
    changes made to 1ed41044e89cba2486995e64992417624222f255.

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Chat } from './components/Chat.js';
-
-const root = createRoot(document.getElementById('root')!);
-root.render(<Chat />);
